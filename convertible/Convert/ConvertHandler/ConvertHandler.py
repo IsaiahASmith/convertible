2570050c122da25ae3273d10bd794cef7317e839
IsaiahASmith <isaiah@clsolutions.ltd>
2021-08-19 22:27:39 -0400

    Fix type hints for Convert

--- convertible/Convert/ConvertHandler/ConvertHandler.py
+++ convertible/Convert/ConvertHandler/ConvertHandler.py
@@ -3,63 +3,65 @@ from typing import Tuple, Any, Iterator
 from convertible.Convertible import Convertible
 
 
-class _ConvertArgsIterator:
-    __slots__ = ("converts",)
+class _InnerArgIterator:
+    """
+    The actual class that handles the iteration of arguments.
+    Unlike the outer class, the index of argument to be check can be modified.
+    This enables Convert and other subclasses to implement Convertibles that can take multiple arguments.
+    """
 
-    def __init__(self, *converts: Convertible):
-        self.converts = converts
+    def __init__(self, convertibles: Tuple[Convertible, ...], *args: Any):
+        self.convertibles = convertibles
+        self.args = args
+        self.index = -1
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.converts})"
+        return f"{self.__class__.__name__}({self.args})"
 
-    def __call__(self, *args: Any) -> Iterator:
-        class _InnerArgIterator:
-            """
-            The actual class that handles the iteration of arguments.
-            Unlike the outer class, the index of argument to be check can be modified.
-            This enables Convert and other subclasses to implement Convertibles that can take multiple arguments.
-            """
+    def __iter__(self) -> "_InnerArgIterator":
+        return self
 
-            def __init__(_self, *args: Convertible):
-                _self.args = args
-                _self.index = -1
+    def get_next(self) -> Any:
+        self.index += 1
+        if self.index < len(self.args):
+            return self.args[self.index]
+        else:
+            raise StopIteration
 
-            def __repr__(_self) -> str:
-                return f"{_self.__class__.__name__}({_self.args})"
+    def undo(self):
+        self.index -= 1
+        if self.index < 0:
+            raise IndexError(f"{self} cannot have an index below zero")
 
-            def __iter__(_self):
-                return _self
+    def __next__(self):
+        arg = self.get_next()
+        if self.index < len(self.convertibles):
+            return self.convertibles[self.index].convert(arg)
+        else:
+            return arg
 
-            def get_next(_self) -> Any:
-                _self.index += 1
-                if _self.index < len(_self.args):
-                    return _self.args[_self.index]
-                else:
-                    raise StopIteration
 
-            def undo(_self):
-                _self.index -= 1
-                if _self.index < 0:
-                    raise IndexError(f"{_self} cannot have an index below zero")
+class _ConvertArgsIterator:
+    __slots__ = ("convertibles",)
 
-            def __next__(_self):
-                arg = _self.get_next()
-                if _self.index < len(self.converts):
-                    return self.converts[_self.index].convert(arg)
-                else:
-                    return arg
+    def __init__(self, *convertibles: Convertible):
+        self.convertibles = convertibles
 
-        return iter(_InnerArgIterator(*args))
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.convertibles})"
+
+    def __call__(self, *args: Any) -> _InnerArgIterator:
+        return iter(_InnerArgIterator(self.convertibles, *args))
 
 
 class _ConvertKwargsIterator:
-    __slots__ = ("converts",)
+    __slots__ = ("convertibles",)
 
-    def __init__(self, **converts: Convertible):
-        self.converts = converts
+    def __init__(self, **convertibles: Convertible):
+        self.convertibles = convertibles
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.converts})"
+        return f"{self.__class__.__name__}({self.convertibles})"
 
     def __call__(self, **kwargs) -> Iterator:
         class _InnerKwargIterator:
@@ -71,8 +73,8 @@ class _ConvertKwargsIterator:
 
             def __iter__(_self):
                 for key, value in kwargs.items():
-                    if key in self.converts:
-                        yield key, self.converts[key].convert(value)
+                    if key in self.convertibles:
+                        yield key, self.convertibles[key].convert(value)
                     else:
                         yield key, value
 
@@ -87,9 +89,9 @@ class ConvertHandler:
         self.kwargs_converter = _ConvertKwargsIterator(**kwargs)
 
     def __repr__(self) -> str:
-        return f"{self.__class__.__name__}({self.args_converter.converts}, {self.kwargs_converter.converts})"
+        return f"{self.__class__.__name__}({self.args_converter.convertibles}, {self.kwargs_converter.convertibles})"
 
-    def __call__(self, *args, **kwargs) -> Tuple[Iterator[Any], Iterator[Tuple[str, Any]]]:
+    def __call__(self, *args, **kwargs) -> Tuple[_InnerArgIterator, Iterator[Tuple[str, Any]]]:
         """
         The args and kwargs passed to this method represent the arguments passed to the inner function
         of the convert descriptor.
