9e52a17662be283a948aeb05c4e4da2e3a60a810
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-08-29 03:45:04 -0400

    Add NewConvetHandler properly

--- convertible/Convert/ConvertHandler/NewConvertHandler.py
+++ convertible/Convert/ConvertHandler/NewConvertHandler.py
@@ -1,40 +1,57 @@
-from typing import Tuple, List, Iterator, Callable, Dict, Optional, Set, Iterable
-from inspect import getfullargspec
+from typing import Tuple, Callable, Dict, Any, Optional, List
 
-from .Iterable import ConvertIterable
+from ..NextArgumentException import NextArgumentException
+from ..RejectArgumentException import RejectArgumentException
+from .FunctionHandler.FunctionHandler import FunctionHandler
+from .FunctionHandler.ConvertibleCallable import ConvertibleCallable
+from .FunctionHandler.FunctionIterator import FunctionIterator
+from .FunctionHandler.ConvertArgument import ConvertArgument
 from convertible.Convertible import Convertible
 
 
-class Convertibles:
-    def __init__(self, *args: Convertible, **kwargs: Convertible):
-        self.args, self.kwargs = args, kwargs
-
-
-class ConvertibleNoneArgumentException(Exception):
-    pass
+class NoMoreArguments:
+    """A class to indicate when there are no more arguments from the iterator"""
 
 
 class ConvertHandler:
-    def __init__(self, function: Callable, convertibles: Convertibles):
-        args, varargs, varkw, _, kwonlyargs, _, _ = getfullargspec(function)
+    def __init__(self, function: Callable, convertible_callable: ConvertibleCallable):
+        self.handler = FunctionHandler.from_function(function, convertible_callable)
 
-        self._argument_convertibles = self._get_argument_convertibles(args, convertibles.args)
-        self._arguments_convertible = self._get_arguments_convertible(varargs, convertibles)
-        self._keyword_only_convertibles = self._get_keyword_only_convertibles(set(kwonlyargs), convertibles.kwargs)
-        self._keyword_arguments_convertible = self._get_keyword_arguments_convertible(varkw, convertibles.kwargs)
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}({self.handler})"
 
-    @property
-    def argument_convertibles(self) -> Tuple[Tuple[str, Convertible], ...]:
-        return self._argument_convertibles
+    def __call__(self, *args, **kwargs) -> Tuple[List[Any], Dict[str, Any]]:
+        function_iterator = self.handler(*args, **kwargs)
+        _args, _kwargs = [], {}
+        for convert_argument in iter(function_iterator):
+            result = self.convert(function_iterator, convert_argument)
+            if (keyword := convert_argument.keyword) is not None:
+                _kwargs.update({keyword: result})
+            else:
+                _args.append(result)
+        return _args, _kwargs
 
-    @property
-    def arguments_convertible(self) -> Iterator[Convertible]:
-        return iter(self._arguments_convertible)
+    def convert(
+        self, function_iterator: FunctionIterator, convert_argument: ConvertArgument
+    ) -> Tuple[Optional[str], Any]:
+        if convert_argument.convertible is None:
+            return convert_argument.keyword, convert_argument.argument
 
-    @property
-    def keyword_only_convertibles(self) -> Dict[str, Convertible]:
-        return self._keyword_only_convertibles
+        return convert_argument.keyword, self._convert(
+            function_iterator, convert_argument.convertible, convert_argument.argument
+        )
 
-    @property
-    def keyword_arguments_convertible(self) -> Dict[str, Convertible]:
-        return self._keyword_arguments_convertible
+    def _convert(self, function_iterator: FunctionIterator, convertible: Convertible, argument: Any):
+        try:
+            result = convertible.convert(argument)
+        except NextArgumentException as exception:
+            try:
+                argument = function_iterator.args.popleft()
+            except IndexError:
+                argument = NoMoreArguments()
+            result = self._convert(function_iterator, exception.convertible, argument)
+        except RejectArgumentException as exception:
+            for argument in exception.rejected_arguments:
+                function_iterator.args.appendleft(argument)
+            result = exception.result
+        return result
